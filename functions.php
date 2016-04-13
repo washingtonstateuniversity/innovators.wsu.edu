183ab128c290616aea0cc76e105e2aa1424cc12c
Jeremy Felt <jeremy.felt@gmail.com>
2016-04-13 16:55:39 -0700

    Account for change to CURIEs in REST API

--- functions.php
+++ functions.php
@@ -24,12 +24,12 @@ add_filter( 'wsu_content_syndicate_host_data', 'innovators_filter_syndicate_host
  * @return object Modified data.
  */
 function innovators_filter_syndicate_host_data( $subset, $post ) {
-	if ( isset( $post->featured_media ) && isset( $post->_embedded->{'https://api.w.org/featuredmedia'} ) && 0 < count( $post->_embedded->{'https://api.w.org/featuredmedia'} ) ) {
-		$subset_feature = $post->_embedded->{'https://api.w.org/featuredmedia'}[0]->media_details;
+	if ( isset( $post->featured_media ) && isset( $post->_embedded->{'wp:featuredmedia'} ) && 0 < count( $post->_embedded->{'wp:featuredmedia'} ) ) {
+		$subset_feature = $post->_embedded->{'wp:featuredmedia'}[0]->media_details;
 		if ( isset( $subset_feature->sizes->{'spine-medium_size'} ) ) {
 			$subset->thumbnail = $subset_feature->sizes->{'spine-medium_size'}->source_url;
 		} else {
-			$subset->thumbnail = $post->_embedded->{'https://api.w.org/featuredmedia'}[0]->source_url;
+			$subset->thumbnail = $post->_embedded->{'wp:featuredmedia'}[0]->source_url;
 		}
 	} else {
 		$subset->thumbnail = false;
